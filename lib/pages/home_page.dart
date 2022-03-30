90c5beea1480be57fc0e51d130f9c2163ffd0754
bogoshipeo18 <bogoshipeo18@gmail.com>
2022-03-30 20:54:27 +0530

    day 13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
 import 'package:flutter_application_1/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
     final int days=30;
+
     final String name="Indo-Korean Cultural Club";
+
+    @override
+  void initState() {    
+    super.initState();
+    loadData();
+  }
+
+  loadData() async{
+   final catalogJson= await rootBundle.loadString("assets/files/Catalog.json.json");
+   final decodedData=jsonDecode(catalogJson);
+   var productsData=decodedData["products"];
+   CatalogModel.items=List.from(productsData).map<Item>((item) => Item.fromMap(item)).toList();
   
+   setState(() {});
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dummyList=List.generate(20,(index)=>CatalogModel.items[0]);
+        
     return Scaffold(
         appBar: AppBar(
           title: Text("IKCC App"),
@@ -17,10 +41,10 @@ class HomePage extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: ListView.builder(
-            itemCount: dummyList.length,
+            itemCount:CatalogModel.items.length,
             itemBuilder: (context,index) {
               return ItemWidget(
-                item:dummyList[index],
+                item:CatalogModel.items[index],
                 );
             },
            
@@ -29,4 +53,4 @@ class HomePage extends StatelessWidget {
         drawer: MyDrawer(),
       );
     }
-  }
+}
